d7f94553f335281fbf2273aff06163cf543c8b44
Thompson818 <tomlampertius@gmail.com>
2017-09-28 19:48:27 -0400

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'clientId'      : 'DISCORD_CLIENT_ID',
+  'clientSecret'  : 'DISCORD_CLIENT_SECRET',
+  'username'      : 'DISCORD_BOT_USERNAME',
+  'botToken'      : 'DISCORD_BOT_TOKEN',
+};
